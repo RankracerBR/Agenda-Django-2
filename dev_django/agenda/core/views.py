30bef21fc01e1a6eddc7137444557732166bec45
RankracerBR <101043200+RankracerBR@users.noreply.github.com>
2022-10-06 12:12:20 -0300

    Django

---
+++ dev_django/agenda/core/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render #, redirect
+from core.models import Evento
+# Create your views here.
+
+#def index(request):
+#    return redirect('/agenda/')
+
+def lista_eventos(request): # função para chamar o arquivo html
+   #usuario = request.user
+   evento = Evento.objects.all()
+   dados = {'eventos': evento}
+   return render(request,'agenda.html', dados)
